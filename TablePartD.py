7632a9444142858c2abe8723af07c2bba2233b47
sabieducation <37680171+sabieducation@users.noreply.github.com>
2018-03-26 20:14:12 -0400

    Update TablePartD.py

--- TablePartD.py
+++ TablePartD.py
@@ -1,2 +1,16 @@
 import happybase as hb
 
+conn = hb.Connection()
+table = conn.table('powers')
+
+row = table.row(b'row1')
+value1 = row[b'personal:hero']
+value2 = row[b'personal:power']
+value3 = row[b'professional:name']
+value4 = row[b'professional:xp']
+value5 = row[b'custom:colors']
+
+print('hero: {} power: {} name: {} xp: {} color: {}'.format(value1, value2, value3, value4, value5))
+
+
+
