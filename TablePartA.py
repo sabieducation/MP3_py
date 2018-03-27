6e3c6cfd2bf8ef4b1c57a6293be270c885a446ff
sabieducation <37680171+sabieducation@users.noreply.github.com>
2018-03-26 20:00:05 -0400

    Update TablePartA.py

--- TablePartA.py
+++ TablePartA.py
@@ -1,2 +1,23 @@
 import happybase as hb
 # Add a new line
+
+conn = hb.Connection()
+
+cf1 = {
+    'personal': dict(),
+    'professional': dict(),
+    'custom': dict()
+}
+
+conn.create_table('powers', cf1)
+
+cf2 = {
+    'nutrition': dict(),
+    'taste': dict()
+}
+
+conn.create_table('food', cf2)
+
+conn.close()
+
+
