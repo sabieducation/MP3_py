1792511073fdbf0bc22b55a3ed4bdd87d94ba032
sabieducation <37680171+sabieducation@users.noreply.github.com>
2018-03-27 00:09:13 -0400

    Update TablePartD.py

--- TablePartD.py
+++ TablePartD.py
@@ -4,13 +4,26 @@ conn = hb.Connection()
 table = conn.table('powers')
 
 row = table.row(b'row1')
-value1 = row[personal:hero]
+value1 = row[b'personal:hero']
 value2 = row[b'personal:power']
 value3 = row[b'professional:name']
 value4 = row[b'professional:xp']
-value5 = row[b'custom:colors']
+value5 = row[b'custom:color']
 
-print('hero: {} power: {} name: {} xp: {} color: {}'.format(value1, value2, value3, value4, value5))
+print('hero: {},  power: {},  name: {},  xp: {},  color: {}'.format(value1, value2, value3, value4, value5))
+
+row = table.row(b'row19')
+value1 = row[b'personal:hero']
+value2 = row[b'custom:color']
+
+print('hero: {},  color: {}'.format(value1, value2))
+
+row = table.row(b'row1')
+value1 = row[b'personal:hero']
+value2 = row[b'professional:name']
+value3 = row[b'custom:color']
+
+print('hero: {},  name: {},  color: {}'.format(value1, value2, value3))
 
 
 
