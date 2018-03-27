ffd0051a5a6e02a6b48fd65cc304e8c6bb7b062b
sabieducation <37680171+sabieducation@users.noreply.github.com>
2018-03-26 20:09:55 -0400

    Update TablePartC.py

--- TablePartC.py
+++ TablePartC.py
@@ -1,3 +1,22 @@
 import happybase as hb
 import csv
 
+f = 'input.csv'
+ufile = open(f)
+reader = csv.reader(ufile, delimiter=',')
+conn = hb.Connection()
+table = conn.table('powers')
+
+i = 0
+for row in reader:
+    i = i + 1
+    print(row)
+    table.put(row[0],
+        {'personal:name': row[1],
+         'personal:power': row[2],
+         'professional:name': row[3],
+         'professional:xp': row[4],
+         'custom:color': row[5]
+})
+
+
